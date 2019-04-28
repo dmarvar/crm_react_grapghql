442771d9fec0cf40904c82ba6ba4dd6117f35f06
Arturo Martinez <arturomartinez@MacBook-Pro-de-Arturo.local>
2019-04-28 01:00:29 -0500

    Creacion de formulario de cliente y elaboracion de mutation funciional

--- cliente/src/components/nuevoCliente.jsx
+++ cliente/src/components/nuevoCliente.jsx
@@ -1,51 +1,114 @@
 import React, { Component, Fragment } from 'react';
+import { NUEVO_CLIENTE } from '../mutations';
+import { Mutation } from 'react-apollo';
 
 export default class NuevoCliente extends Component {
-	state = {};
+	state = {
+		cliente: {
+			nombre: '',
+			apellido: '',
+			empresa: '',
+			tipo: '',
+			edad: ''
+		}
+	};
+	changeValue = (e) => {
+		this.setState({ cliente: { ...this.state.cliente, [e.currentTarget.name]: e.currentTarget.value } });
+	};
+	submitForm = (crearCliente, e) => {
+		e.preventDefault();
+		const input = { ...this.state.cliente, edad: Number(this.state.cliente.edad) };
+		crearCliente({ variables: { input } });
+	};
 	render() {
+		const cliente = this.state;
 		return (
 			<Fragment>
 				<h2 className="text-center">Nuevo Cliente</h2>
 				<div className="row justify-content-center">
-					<form className="col-md-8 m-3">
-						<div className="form-row">
-							<div className="form-group col-md-6">
-								<label>Nombre</label>
-								<input type="text" className="form-control" placeholder="Nombre" />
-							</div>
-							<div className="form-group col-md-6">
-								<label>Apellido</label>
-								<input type="text" className="form-control" placeholder="Apellido" />
-							</div>
-						</div>
-						<div className="form-row">
-							<div className="form-group col-md-6">
-								<label>Empresa</label>
-								<input type="text" className="form-control" placeholder="Empresa" />
-							</div>
-							<div className="form-group col-md-6">
-								<label>Email</label>
-								<input type="email" className="form-control" placeholder="Email" />
-							</div>
-						</div>
-						<div className="form-row">
-							<div className="form-group col-md-6">
-								<label>Edad</label>
-								<input type="text" className="form-control" placeholder="Edad" />
-							</div>
-							<div className="form-group col-md-6">
-								<label>Tipo Cliente</label>
-								<select className="form-control">
-									<option value="">Elegir...</option>
-									<option value="PREMIUM">PREMIUM</option>
-									<option value="BASICO">BÁSICO</option>
-								</select>
-							</div>
-						</div>
-						<button type="submit" className="btn btn-success float-right">
-							Guardar Cambios
-						</button>
-					</form>
+					<Mutation mutation={NUEVO_CLIENTE}>
+						{(crearCliente) => (
+							<form className="col-md-8 m-3" onSubmit={this.submitForm.bind(this, crearCliente)}>
+								<div className="form-row">
+									<div className="form-group col-md-6">
+										<label>Nombre</label>
+										<input
+											type="text"
+											className="form-control"
+											placeholder="Nombre"
+											name="nombre"
+											onChange={this.changeValue}
+											value={cliente.nombre}
+										/>
+									</div>
+									<div className="form-group col-md-6">
+										<label>Apellido</label>
+										<input
+											type="text"
+											className="form-control"
+											placeholder="Apellido"
+											name="apellido"
+											onChange={this.changeValue}
+											value={cliente.apellido}
+										/>
+									</div>
+								</div>
+								<div className="form-row">
+									<div className="form-group col-md-6">
+										<label>Empresa</label>
+										<input
+											type="text"
+											className="form-control"
+											placeholder="Empresa"
+											name="empresa"
+											onChange={this.changeValue}
+											value={cliente.empresa}
+										/>
+									</div>
+									<div className="form-group col-md-6">
+										<label>Email</label>
+										<input
+											type="email"
+											className="form-control"
+											placeholder="Email"
+											name="email"
+											onChange={this.changeValue}
+											value={cliente.email}
+										/>
+									</div>
+								</div>
+								<div className="form-row">
+									<div className="form-group col-md-6">
+										<label>Edad</label>
+										<input
+											type="number"
+											className="form-control"
+											placeholder="Edad"
+											name="edad"
+											onChange={this.changeValue}
+											value={cliente.edad}
+										/>
+									</div>
+									<div className="form-group col-md-6">
+										<label>Tipo Cliente</label>
+										<select
+											className="form-control"
+											name="tipo"
+											onChange={this.changeValue}
+											value={cliente.tipo}
+										>
+											<option value="">Elegir...</option>
+											<option value="PREMIUM">PREMIUM</option>
+											<option value="BASICO">BÁSICO</option>
+										</select>
+									</div>
+								</div>
+								<button type="submit" className="btn btn-success float-right">
+									Guardar Cambios
+								</button>
+							</form>
+						)}
+					</Mutation>
 				</div>
 			</Fragment>
 		);
